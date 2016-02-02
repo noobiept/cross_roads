e8ee22f3d50edb36aedb17af6e2760c9499f1185
Pedro Ferreira <plferreira4@gmail.com>
2016-02-02 16:49:00 +0000

    - add a gruntjs build script - remove other build script

---
+++ build/Gruntfile.js
@@ -0,0 +1,91 @@
+var PATH = require( 'path' );
+
+module.exports = function( grunt )
+{
+var root = '../';
+var dest = '../release/<%= pkg.name %> <%= pkg.version %>/';
+
+grunt.initConfig({
+        pkg: grunt.file.readJSON( 'package.json' ),
+
+            // delete the destination folder
+        clean: {
+            options: {
+                force: true
+            },
+            release: [
+                dest
+            ]
+        },
+
+            // copy the necessary files
+        copy: {
+            release: {
+                expand: true,
+                cwd: root,
+                src: [
+                    'images/**',
+                    'levels/**',
+                    'libraries/**',
+                    'music/**',
+                    'background.js',
+                    'manifest.json'
+                ],
+                dest: dest
+            }
+        },
+
+        uglify: {
+            release: {
+                files: {
+                    '../release/<%= pkg.name %> <%= pkg.version %>/min.js': [
+                        '../scripts/app_storage.js',
+                        '../scripts/car.js',
+                        '../scripts/player.js',
+                        '../scripts/road.js',
+                        '../scripts/level.js',
+                        '../scripts/keyboard.js',
+                        '../scripts/high_score.js',
+                        '../scripts/game_menu.js',
+                        '../scripts/options.js',
+                        '../scripts/game.js',
+                        '../scripts/main.js'
+                    ]
+                }
+            }
+        },
+
+        cssmin: {
+            release: {
+                files: [{
+                    expand: true,
+                    cwd: root + 'css',
+                    src: 'style.css',
+                    dest: dest + 'css/'
+                }]
+            }
+        },
+
+        processhtml: {
+            release: {
+                files: [{
+                    expand: true,
+                    cwd: root,
+                    src: 'index.html',
+                    dest: dest
+                }]
+            }
+        }
+    });
+
+
+    // load the plugins
+grunt.loadNpmTasks( 'grunt-contrib-copy' );
+grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+grunt.loadNpmTasks( 'grunt-contrib-clean' );
+grunt.loadNpmTasks( 'grunt-processhtml' );
+
+    // tasks
+grunt.registerTask( 'default', [ 'clean', 'copy', 'uglify', 'cssmin', 'processhtml' ] );
+};
