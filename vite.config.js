5b260ff958525e417a11fef3a1f125f4f7437677
Pedro Ferreira <plferreira4@gmail.com>
2024-06-30 23:57:30 +0200

    🐛: Use relative paths when building.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import packageJson from "./package.json";
 
 export default defineConfig({
+    base: "",
     build: {
         outDir: `release/${packageJson.name} ${packageJson.version}`,
     },
